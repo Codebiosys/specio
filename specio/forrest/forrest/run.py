1f8c637b607a13925e35fbe7e4fc78789b6d68f9
Brian Schrader <brian@brianschrader.com>
2018-10-17 10:46:31 -0700

    Adds new logging configs, CLI, and run modes.
    Specio has a new runmode where it can idle and wait for commands. This
    is now the default mode.
    
    To interact with the new idle process, either use the Docker (vanilla)
    commands to exec into the running container, or use the new specio-cli
    script to quickly execute commands in the specio head node.
    
    Logging has also been fixed and should more reliably print to the kibana
    logs now.

--- specio/forrest/forrest/run.py
+++ specio/forrest/forrest/run.py
@@ -1,4 +1,5 @@
 import logging
+from logging import getLogger
 import logging.config
 import time
 import yaml
@@ -9,6 +10,13 @@ from .configuration import Configuration
 from .handlers import SpecioEventHandler
 from .tasks import pipeline
 
+
+WATCH_MODE, WORKER_MODE, IDLE_MODE, PLAIN_MODE = (
+  'watch',
+  'worker',
+  'idle',
+  'plain'
+)
 
 logger = logging.getLogger(__name__)
 
@@ -40,14 +48,49 @@ def get_observer(config, handler):
     return observer
 
 
-def main(run_once=False):
+def main(mode=PLAIN_MODE, run_once=False):
     """ A method that watches files for changes and runs the full pipline. """
     config = Configuration()
     setup_logging(config)
 
     logger.info('Welcome to Specio!')
 
-    if run_once:
+    # Start the app as a worker
+    if mode == WORKER_MODE:
+        from .celery import app
+        app.worker_main()
+
+    # Watch for file changes
+    elif mode == WATCH_MODE:
+        event_handler = get_event_handler(config)
+        observer = get_observer(config, event_handler)
+
+        logger.info(
+            'In this mode, Specio will watch {config.path} for changes invoke the '
+            'pipeline each time a ".yml" file changes.'
+        )
+
+        observer.start()
+
+        logger.info('Entering runloop. Watching for changes...')
+
+        try:
+            while True: time.sleep(1)  # noqa
+        except KeyboardInterrupt:
+            logger.info('Runloop cancelled by user. Exiting.')
+            observer.stop()
+
+        observer.join()
+
+    # Just sit and wait
+    elif mode == IDLE_MODE:
+        try:
+            while True: time.sleep(1)  # noqa
+        except KeyboardInterrupt:
+            logger.info('Runloop cancelled by user. Exiting.')
+
+    # Manual run once
+    elif run_once:
         logger.info(
             'In this mode, Specio will not watch for file changes and must instead '
             'be invoked each time you want to re-run the pipline.'
@@ -61,26 +104,7 @@ def main(run_once=False):
         result.get()
 
         logger.info('Pipeline complete! Exiting.')
-        return
 
-    # In the normal run-forever mode.
-
-    event_handler = get_event_handler(config)
-    observer = get_observer(config, event_handler)
-
-    logger.info(
-        'In this mode, Specio will watch {config.path} for changes invoke the '
-        'pipeline each time a ".yml" file changes.'
-    )
-
-    observer.start()
-
-    logger.info('Entering runloop. Watching for changes...')
-
-    try:
-        while True: time.sleep(1)  # noqa
-    except KeyboardInterrupt:
-        logger.info('Runloop cancelled by user. Exiting.')
-        observer.stop()
-
-    observer.join()
+    # No valid run mode specified. Skipping.
+    else:
+        pass
